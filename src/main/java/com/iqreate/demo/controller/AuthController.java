76d5e3f79dc064d1b35922210e959c675fa697f7
Antarjeet <antarjeetpradhan@gmail.com>
2025-08-08 17:34:35 +0530

    This is debugged folder in Spring security JWT

---
+++ src/main/java/com/iqreate/demo/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.iqreate.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import com.iqreate.demo.dto.LoginRequest;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                loginRequest.getEmail(),
+                loginRequest.getPassword()
+            )
+        );
+
+        String token = "dummy-jwt-token";
+        return ResponseEntity.ok(Collections.singletonMap("token", token));
+    }
+}
